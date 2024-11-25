9c4f7bc7d5dba1558b689a4428f95a5e908be4ed
ashik <ashik123ks@gmail.com>
2024-11-25 10:41:50 +0530

    Ready to deploy

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require('cors');
+const fs = require('fs');
+const https = require('https')
 
 dotenv.config();
 
@@ -14,7 +16,8 @@ mongoConnect();
 
 app.use(cors());  // Enable CORS for API requests
 const axios = require('axios');
-app.use(express.static(path.join(__dirname, '../client')));  // Serve static files correctly
+// app.use(express.static(path.join(__dirname, '../client')));  // Serve static files correctly
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.urlencoded({ extended: true }));
@@ -26,6 +29,9 @@ app.get('/test', (req, res) => {
     res.status(200).send('Test successful');
 });
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running at http://localhost:${process.env.PORT}`);
-});
+https.createServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+  }, app).listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+  });
